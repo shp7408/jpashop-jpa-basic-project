c58903637703af1a8e08727527ff2a70c9c7d505
shp7408 <shp7408@naver.com>
2022-12-08 16:52:04 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("Hello");
+		String data = hello.getData();
+
+		/*
+		* hellp.getD.. 치고난 후, control + option + v 단축키 누르면,
+		* String data = 부분을 자동완성 해준다. extract variable 기능
+		 * */
+		System.out.println("data = " + data );
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
